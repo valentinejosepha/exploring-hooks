41c1710d52cd91e25fa6cfc15ffc7726061cfbdd
Valentine Voyen <valentine.voyen1@gmail.com>
2019-11-22 11:45:24 +0100

    creating a typerace game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userText, setUserText] = useState('');
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    console.log('current userText', userText);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Type Race</h2>
+      <input value={userText} onChange={updateUserText} />
     </div>
   );
 }
